ab9c00067968873d26535496316306532ea50b3b
Piyush12379 <150358735+Piyush12379@users.noreply.github.com>
2026-02-13 09:11:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Max sum subarray by removing at most one element/max-sum-subarray-by-removing-at-most-one-element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int maxSumSubarray(vector<int>& arr) {
+        // Code here
+        int deleteone=arr[0];
+        int nodelete=arr[0];
+        int n=arr.size();
+        int ans=arr[0];
+        for(int i=1;i<n;i++){
+            int prevnodelete=nodelete;
+            nodelete=max(nodelete+arr[i],arr[i]);
+            deleteone=max(deleteone+arr[i],prevnodelete);
+            ans=max({ans,nodelete,deleteone});
+        } 
+        return ans;
+        
+    }
+};
